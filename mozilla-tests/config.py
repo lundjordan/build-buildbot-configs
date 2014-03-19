cb97a05f74208f6e912c60d4ad0e9a0763e65889
Jordan Lund <jordan.l.lund@gmail.com>
2014-03-19 14:32:20 -0700

    omtc win8 fix

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -1796,10 +1796,10 @@ BRANCHES['cedar']['platforms']['macosx64']['mavericks']['opt_unittest_suites'] =
 BRANCHES['cedar']['platforms']['macosx64']['mavericks']['debug_unittest_suites'] = UNITTEST_SUITES['debug_unittest_suites'][:]
 BRANCHES['cedar']['platforms']['win32']['xp-ix']['opt_unittest_suites'] = UNITTEST_SUITES['opt_unittest_suites'] + REFTEST_OMTC
 BRANCHES['cedar']['platforms']['win32']['win7-ix']['opt_unittest_suites'] = UNITTEST_SUITES['opt_unittest_suites'] + REFTEST_OMTC
-BRANCHES['cedar']['platforms']['win32']['win8-ix']['opt_unittest_suites'] = UNITTEST_SUITES['opt_unittest_suites'] + REFTEST_OMTC
+BRANCHES['cedar']['platforms']['win32']['win8']['opt_unittest_suites'] = UNITTEST_SUITES['opt_unittest_suites'] + REFTEST_OMTC
 BRANCHES['cedar']['platforms']['win32']['xp-ix']['debug_unittest_suites'] = UNITTEST_SUITES['debug_unittest_suites'] + REFTEST_OMTC
 BRANCHES['cedar']['platforms']['win32']['win7-ix']['debug_unittest_suites'] = UNITTEST_SUITES['debug_unittest_suites'] + REFTEST_OMTC
-BRANCHES['cedar']['platforms']['win32']['win8-ix']['debug_unittest_suites'] = UNITTEST_SUITES['debug_unittest_suites'] + REFTEST_OMTC
+BRANCHES['cedar']['platforms']['win32']['win8']['debug_unittest_suites'] = UNITTEST_SUITES['debug_unittest_suites'] + REFTEST_OMTC
 
 # Disable mochitest-browser-chrome on mozilla-b2g branches
 for branch in [x for x in BRANCHES.keys() if x.startswith('mozilla-b2g')]:
